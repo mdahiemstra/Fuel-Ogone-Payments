70151dbf1c99d44a2af589a87a970bc521662d31
Michel Hiemstra <michel@letsblinq.com>
2011-11-02 06:17:57 +0100

    Updated config file
    Signed-off-by: Michel Hiemstra <michel@letsblinq.com>

--- config/ogone.php
+++ config/ogone.php
@@ -17,11 +17,11 @@
 
 return array(
 	'debug' => true,
-	'psp_id' => 'test',
-	'shasign' => ')i8YKLod$j4&AOu3pfy', // SHA-IN Signature
+	'psp_id' => 'your_psp_id',
+	'shasign' => 'i8YKLod$j4&AOu3pfy', // SHA-IN Signature
 	'hash_method' => MHASH_SHA512, // MHASH_SHA1, MHASH_SHA256 or MHASH_SHA512 depending on your Ogone configuration
 	'language' => 'en_US', // en_US, nl_NL, fr_FR, ...
-	'currency' => 'USD', // ISO currency code
+	'currency' => 'EUR', // ISO currency code
 	'accept_url' => \Uri::create(\Uri::current() . '/status/accepted'),
 	'decline_url' => \Uri::create(\Uri::current() . '/status/declined'),
 	'exception_url' => \Uri::create(\Uri::current() . '/status/exception'),
